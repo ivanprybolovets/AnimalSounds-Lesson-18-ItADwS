566a96174f6ba4b2dbca4c1ed4cf194387ff69a0
Ivan Prybolovetz <iprybolovetz@gmail.com>
2020-04-03 13:38:17 +0300

    Feature sound to Button

--- AnimalSounds Lesson 18 ItADwS/ViewController.swift
+++ AnimalSounds Lesson 18 ItADwS/ViewController.swift
@@ -10,14 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let meowSound = SimpleSound(named: "meow")
+    let woofSound = SimpleSound(named: "woof")
+    let mooSound = SimpleSound(named: "moo")
+    
+    @IBOutlet weak var animalSoundLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
     @IBAction func catButtonTapped(_ sender: UIButton) {
+        animalSoundLabel.text = "Meow"
+        meowSound.play()
     }
     @IBAction func dogButtonTapped(_ sender: UIButton) {
+        animalSoundLabel.text = "Woof"
+        woofSound.play()
     }
     @IBAction func cowButtonTapped(_ sender: UIButton) {
+        animalSoundLabel.text = "Moo"
+        mooSound.play()
     }
     
 }
